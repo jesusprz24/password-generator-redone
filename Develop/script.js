f577b6b6763cc1feaa1fb5f78d98df1ec0a7687f
Jesus <perez.jesus1039@gmail.com>
2023-03-21 18:07:29 -0700

    changed the flow of the original document, added the length variable and alert to allow users to input what they want

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,11 @@
-// Assignment code here
+const generateButton = document.getElementById("generate");
+const passwordTextarea = document.getElementById("password");
 
+generateButton.addEventListener("click", () => {
+  const length = prompt("Enter password length between 8 and 128 characters:");
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  if (!length || length < 8 || length > 128) {
+    alert("Password length must be a number between 8 and 128");
+    return;
+  }
+})
